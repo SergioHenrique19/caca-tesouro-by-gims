3f514b47126b8c547cd5b3610496c450a576e3d0
SergioHenrique19 <garciasergiodm@gmail.com>
2019-12-03 13:47:52 -0300

    :hammer: Adiciona codigo devidamente documentado - close #3

--- src/Analisador.java
+++ src/Analisador.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
 
+/**
+ * Classe responsável por analisar o que foi digitado pelo usuário e retorna um
+ * objeto do tipo Comando, caso seja válido, e, caso contrário, retorna um
+ * objeto nulo. Quando chamado, lê uma linha do terminal e tenta interpretar
+ * como um comando do jogo.
+ */
 public class Analisador {
 	private PalavrasComando palavrasDeComando;
 	private Scanner entrada;
 
+	/**
+	 * Cria um analisador para ler o que foi digitado no terminal.
+	 */
 	public Analisador() {
 		palavrasDeComando = new PalavrasComando();
 		entrada = new Scanner(System.in);
 	}
 
+	/**
+	 * Metodo que analisa o comando digitado pelo usuário e verifica se válido.
+	 * Inicialmente, pega a primeira linha e analisa se tem uma ou duas palavras. Em
+	 * seguida, verifica se a primeira palavra digita é um comando válido.
+	 * 
+	 * @return Objeto Comando válido se a primeira palavra for comando válido, ou
+	 *         Comando desconhecido se a primeira não for válida.
+	 */
 	public Comando pegarComando() {
 		String linha;
 		String palavra1 = null;
@@ -20,6 +37,7 @@ public class Analisador {
 
 		Scanner tokenizer = new Scanner(linha);
 
+		// Verifica se tem uma ou duas palavras, e ignora o resto da linha
 		if (tokenizer.hasNext()) {
 			palavra1 = tokenizer.next();
 			if (tokenizer.hasNext()) {
@@ -29,19 +47,28 @@ public class Analisador {
 
 		tokenizer.close();
 
+		// Verifica se a primeira palavra é um comando válido
 		if (palavrasDeComando.ehComando(palavra1)) {
 			return new Comando(palavra1, palavra2);
-		}
-
-		else {
+		} else {
 			return new Comando(null, palavra2);
 		}
 	}
 
+	/**
+	 * Método que recebe comando (String) digitada pelo usuário para analisar.
+	 * 
+	 * @return Comando (String) digitado pelo usuário
+	 */
 	public String pegarString() {
 		return entrada.nextLine();
 	}
 
+	/**
+	 * Método que apresenta os comandos válidos para o usuário.
+	 * 
+	 * @return Vetor dos comandos válidos
+	 */
 	public String getComandos() {
 		return palavrasDeComando.getComandos();
 	}
